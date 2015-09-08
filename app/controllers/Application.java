bb9d109152924cf6f3b184782ce0b3732b696ed8
unknown <kakus@LKAKUSW7BLR.rpega.com>
2015-09-08 21:33:28 +0530

    persons

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,5 +10,10 @@ public class Application extends Controller {
     public Result index() {
         return ok(index.render("Your new application is ready."));
     }
+    
+    public Result getPerson(String name , int age)
+    {
+    	return ok(persons.render(name , age));
+    }
 
 }
